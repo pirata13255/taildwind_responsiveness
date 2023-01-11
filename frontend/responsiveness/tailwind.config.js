0be49dd69c6964aae3eecd713bd031c94a5f8899
pirata13255 <miguelsilva2001511@gmail.com>
2023-01-11 18:32:59 +0000

    finnished design

--- frontend/responsiveness/tailwind.config.js
+++ frontend/responsiveness/tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens:{
+      xs:"540px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
